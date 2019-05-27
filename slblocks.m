4b5bdb9a7a92268e3023230821fefa7fbb0b6ab1
anontruck <zack91smith@gmail.com>
2019-05-26 17:13:30 -0700

    Initial commit.

---
+++ slblocks.m
@@ -0,0 +1,13 @@
+function blkStruct = slblocks
+		% This function specifies that the library should appear
+		% in the Library Browser
+		% and be cached in the browser repository
+
+		Browser.Library = 'EE237LIB';
+		% 'mylib' is the name of the library
+
+		Browser.Name = 'EE237 Lib';
+		% 'My Library' is the library name that appears 
+             % in the Library Browser
+
+		blkStruct.Browser = Browser; 
